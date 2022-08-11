81e43c70470c460f220e6f2c12e59d0eb33c418d
eagalpb <gabriella.pal@ericsson.com>
2022-08-11 16:16:52 +0200

    Finish auth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,15 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    // passport automatically attaches logout function to the req object
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    // passport automatically attaches user to the req object
+    res.send(req.user);
+  });
 };
